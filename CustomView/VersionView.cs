128982600d64da81bcb79bfbd2a4f35e45985fab
ali2 <ali62n62@yahoo.com>
2018-02-12 15:14:04 +0330

    ok work

--- CustomView/VersionView.cs
+++ CustomView/VersionView.cs
@@ -1,16 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
 using Android.Content;
 using Android.Content.PM;
-using Android.Graphics;
-using Android.OS;
 using Android.Runtime;
 using Android.Util;
-using Android.Views;
 using Android.Widget;
 
 namespace CustomComponents.CustomView
--- Draw/Pizza.cs
+++ Draw/Pizza.cs
@@ -1,17 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
 using Android.Content;
 using Android.Content.Res;
 using Android.Graphics;
-using Android.OS;
-using Android.Runtime;
 using Android.Util;
 using Android.Views;
-using Android.Widget;
 
 namespace CustomComponents.Draw
 {
